e387781a0c89c8b2b52078aab08b5d6f710ce892
sahadatsonar-cmyk <sahadatsonar@gmail.com>
2026-09-09 12:36:19 +0600

    feat(phase8): export coding tools

--- packages/agent-core/src/tools/index.ts
+++ packages/agent-core/src/tools/index.ts
@@ -4,3 +4,6 @@ export { calculatorTool } from './calculator';
 export { datetimeTool } from './datetime';
 export { webFetchTool } from './web-fetch';
 export { webSearchTool } from './web-search';
+export { codeAnalyzeTool, proposePatchTool } from './code';
+export { githubGetFileTool, githubListDirTool } from './github';
+export { shellTool } from './shell';
