2812268cb626b058af5e12c76c88d8b353b73e9f
sahadatsonar-cmyk <sahadatsonar@gmail.com>
2026-09-09 11:14:09 +0600

    feat(tools): export tools module

---
+++ packages/agent-core/src/tools/index.ts
@@ -0,0 +1,6 @@
+export * from './types';
+export * from './registry';
+export { calculatorTool } from './calculator';
+export { datetimeTool } from './datetime';
+export { webFetchTool } from './web-fetch';
+export { webSearchTool } from './web-search';
